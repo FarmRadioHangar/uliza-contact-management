5ee2b772ee3bc54c4456c3bcb0f58ccdf3a8fe03
henry14 <henrytebs@gmail.com>
2018-07-16 15:54:54 +0300

    Renamed url to urls

--- contactmanager/urls.py
+++ contactmanager/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('contacts.url')),
+    #url(r'^', include('contacts.urls', namespace='uliza-contacts', app_name='contacts')),
+    url(r'^', include('contacts.urls')),
 ]
